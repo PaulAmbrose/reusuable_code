4695465867f34c29473c5880cfd3553520195889
Paul Ambrose <pablo_rose34@hotmail.com>
2022-10-10 19:17:42 +0100

    Create pygame_drawing_pad.py

---
+++ python/pygame_drawing_pad.py
@@ -0,0 +1,14 @@
+#Small program to help with testing objects being drawn in pygame framework
+
+from pygame import *
+
+#colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#window parameters
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
