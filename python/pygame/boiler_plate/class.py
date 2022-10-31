2ddfb58432210e913b9be653764b0a53dd77e4fb
Paul Ambrose <pablo_rose34@hotmail.com>
2022-10-31 20:08:34 +0000

    Create class.py

---
+++ python/pygame/boiler_plate/class.py
@@ -0,0 +1,19 @@
+# Desc
+# Paul Ambrose - dd/mm/yyyy
+
+class X():
+
+    def __init__(self, aA, bB, cC, dD, eE):
+
+    #definition of attributes
+        self.aA = aA,
+        self.bB = bB,
+        self.cC = cC,
+        self.dD = dD,
+        self.eE = eE
+        self.fF = 100
+
+    #definition of methods
+    def product(self):
+        valueProducts = self.aA * self.bB * self.cC * self.dD * self.eE * self.fF
+        return valueProducts
